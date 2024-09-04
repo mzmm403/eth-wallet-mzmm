a7a4c9b0b4b6b71b9154596aa5277bf8669d25f3
老托尔所啦 <9895382+old-tol@user.noreply.gitee.com>
2024-09-04 21:59:33 +0800

    Packaged deployment :package:

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { basename } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/eth-wallet-mzmm/', // github仓库名称
   plugins: [
     vue(),
     nodePolyfills({
